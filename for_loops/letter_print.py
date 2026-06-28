edd436767a4c6aaa6e5e612121565ce011a3184e
keen06-eng <ashleygatsi80@gmail.com>
2026-06-28 23:47:38 +0200

    Add exercises on for loops

---
+++ for_loops/letter_print.py
@@ -0,0 +1,10 @@
+#for printing the letter F using "*" an for loops
+dimensions=[5,2,5,2,2]
+for dim in dimensions:
+    print("*"*dim)
+
+print("")
+#printing the letter L
+dims=[2,2,2,2,5,5]
+for d in dims:
+    print("*"*d)
---
+++ for_loops/shopping_total.py
@@ -0,0 +1,5 @@
+prices=[10,30,50,80,22]
+total_price=0
+for price in prices:
+    total_price+=price
+print(f"The total price is ${total_price}")
